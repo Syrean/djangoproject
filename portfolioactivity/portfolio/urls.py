9ee7fdbfb7ec74e283e0185d9e05c6ade3431150
Syrean <147214908+Syrean@users.noreply.github.com>
2025-02-21 23:03:35 +0800

    Activity 2 ( With Dashboard)
    Activity 2 Finished

--- portfolioactivity/portfolio/urls.py
+++ portfolioactivity/portfolio/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import index 
 from .views import portf 
 from .views import cont 
+from .views import dashboard 
+from .views import report
+from .views import settings
 
 app_name = "portfolio" 
 
@@ -9,4 +12,7 @@ urlpatterns = [
     path('', index, name='index'), 
     path('portfolio/', portf, name='portf'), 
     path('contacts/', cont, name='cont'), 
+    path('dashboard/', dashboard, name='dashboard'),
+    path('report/', report, name='report'),
+    path('settings/', settings, name='settings')
 ]
--- portfolioactivity/portfolio/views.py
+++ portfolioactivity/portfolio/views.py
@@ -6,3 +6,14 @@ def portf(request):
     return render(request, "pages/portfolio.html")
 def cont(request): 
     return render(request, "pages/contact.html")
+def report(request): 
+    return render(request, "pages/report.html")
+def settings(request): 
+    return render(request, "pages/settings.html")
+def dashboard(request):
+    data = [
+        {"title": "Users", "count": 150},
+        {"title": "Orders", "count": 320},
+        {"title": "Revenue", "count": "12450"},
+        ]
+    return render(request, "pages/dashboard.html", context={"data": data})
